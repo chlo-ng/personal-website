6232d2f21038e99d005665f2e5104d90e70440bb
chloe <chloeng73@gmail.com>
2022-08-17 21:04:31 -0400

    testing netlify

--- personal-website/src/components/home/Home.jsx
+++ personal-website/src/components/home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div id='home'>
             <div className='container home__container'>
-                <h3> Hello, I am </h3>
+                <h3> Hello, </h3>
                 <h1> Chloe Ng. </h1>
                 <p> I am currently a Junior in <b>Georgia Tech</b>, 
                     <br /> pursuing a Bachelor's degree in <b> Computer Science</b>.
